22bd4e5d1df09987235d7912d708d7d0cb58d5fa
Robert Adrian Bucur <adrian_bucur49@ymail.com>
2020-09-21 11:36:59 +0300

    Add API

---
+++ FarmApi/FarmApi/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FarmApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        private FarmEntities _context;
+
+        public IHttpActionResult GetAllUsers()
+        {
+            _context = new FarmEntities();
+            try
+            {
+                dynamic users = _context.GetAllUsers().ToList();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }        
+        
+        public IHttpActionResult AddUser([FromBody] dynamic client)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                String username = client["Username"];
+                String password = client["Password"];
+                String name = client["Name"];
+                String address = client["Address"];
+                String cnp = client["CNP"];
+                String email = client["Email"];
+                _context.AddUser(username, password, name, cnp, address, email, 0);
+                return Ok("User added successfully!");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public IHttpActionResult DeleteUser(string username)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                _context.DeleteUser(username);
+                return Ok("Deleted");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+
+        //public IHttpActionResult GetUserByUsername(String username)
+        //{
+        //    _context = new FarmEntities();
+        //    try
+        //    {
+        //        //var user = _context.GetUserByUsername(username).FirstOrDefault();
+        //        //return Ok(user);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return InternalServerError(ex);
+        //    }
+        //}
+    }
+}
---
+++ FarmApi/FarmApi/Controllers/WorkerController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FarmApi.Controllers
+{
+    public class WorkerController: ApiController
+    {
+        private FarmEntities _context;
+
+        public IHttpActionResult GetAllWorkers()
+        {
+            _context = new FarmEntities();
+            try
+            {
+                dynamic workers = _context.GetAllWorkers().ToList();
+                return Ok(workers);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public IHttpActionResult AddWorker([FromBody] dynamic client)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                String name = client["Username"];
+                String qRCode = client["QRCode"];
+                _context.AddWorker(name, qRCode);
+                return Ok("Worker added successfully!");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public IHttpActionResult GetQuantitiesByWorker([FromBody] dynamic client)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                String qRCode = client["QRCode"];
+                var quantities = _context.GetQuantitiesByWorker(qRCode);
+                return Ok(quantities);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public IHttpActionResult GetTotalQuantityByWorker([FromBody] dynamic client)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                String qRCode = client["QRCode"];
+                var quantity = _context.GetTotalQuantityByWorker(qRCode);
+                return Ok(quantity);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public IHttpActionResult DeleteWorker(string qrCode)
+        {
+            _context = new FarmEntities();
+            try
+            {
+                _context.DeleteWorker(qrCode);
+                return Ok("Deleted");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
